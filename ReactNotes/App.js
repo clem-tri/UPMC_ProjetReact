df409cbaf089b15cd14393d0081746fff983c993
Nashunn <boullet.nicolas@gmail.com>
2018-04-05 14:38:47 +0200

    Test : note display

--- ReactNotes/App.js
+++ ReactNotes/App.js
@@ -1,23 +1,60 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react';
+import { Text, View, ScrollView, StyleSheet } from 'react-native';
+import { Constants } from 'expo';
 
-export default class App extends React.Component {
+class Note extends Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
-      </View>
+      <View style={styles.note}>
+        <Text style={styles.titleNote}>
+          {this.props.title}
+        </Text>
+        <Text>
+        </Text>
+      </View>   
+    );
+  }
+}
+
+export default class App extends Component {
+  render() {
+    return (
+      <ScrollView contentContainerStyle={styles.container}>
+        <Note title='Titre de la note' />
+        <Note title='Titre de la note' />
+        <Note title='Titre de la note' />
+        <Note title='Titre de la note' />
+        <Note title='Titre de la note' />
+        <Note title='Titre de la note' />
+        <Note title='Titre de la note' />
+        <Note title='Titre de la note' />
+      </ScrollView>
     );
   }
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    alignItems: 'flex-start',
+    justifyContent: 'space-around',
+    paddingTop: Constants.statusBarHeight + 20,
+  },
+  note: {
+    width: '46%',
+    marginRight: '2%',
+    marginLeft: '2%',
+    marginBottom: 5,
+    marginTop: 5,
+    backgroundColor: '#555',
+    aspectRatio: 1,
+  },
+  titleNote: {
+    margin: 5,
+    fontSize: 15,
+    fontWeight: 'bold',
+    color: '#fff',
+    textAlign: 'center',
   },
 });
